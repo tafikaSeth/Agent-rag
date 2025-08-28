7cb9c41f5b428842aea1dcc0933a3524d0365b74
Seth TAFIKA <tafikaseth@gmail.com>
2025-08-28 16:14:12 +0300

    Mise à jour du document

--- education-back/app.js
+++ education-back/app.js
@@ -1,14 +1,10 @@
-import { ChatGroq  } from "@langchain/groq";
+import { ChatGroq } from "@langchain/groq";
 import { ChatPromptTemplate } from "@langchain/core/prompts";
-import { Document } from "@langchain/core/documents";
 import { createStuffDocumentsChain } from "langchain/chains/combine_documents";
 import { PDFLoader } from "@langchain/community/document_loaders/fs/pdf";
-import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
-import { OllamaEmbeddings } from "@langchain/ollama";
-import { MemoryVectorStore } from "langchain/vectorstores/memory";
-import { createRetrievalChain } from "langchain/chains/retrieval";
 import express from 'express';
 import cors from 'cors';
+import 'dotenv/config';
 
 const app = express();
 app.use(cors());
@@ -18,49 +14,41 @@ const llm = new ChatGroq({
   model: "llama-3.3-70b-versatile",
   apiKey: process.env.GROQ_API_KEY,
   temperature: 0.7,
-})
+});
 
-const loader = new PDFLoader("./seth.pdf")
+const loader = new PDFLoader("./seth.pdf");
 const docs = await loader.load();
 
 const prompt = ChatPromptTemplate.fromTemplate(
-  `Réponds à la question de l'utilisateur. N'utilise pas de contexte juste la réponse.
-    Context: {context}
-    Question: {input}
- `
-)
+  `En te basant uniquement sur le contexte fourni, réponds précisément à la question de l'utilisateur.
+Contexte : {context}
+Question : {input}
+Réponse :`
+);
 
 const chain = await createStuffDocumentsChain({
   llm,
   prompt,
-})
+});
 
-app.post('/ask', async(req, res) => {
-  const { question } = req.body
+app.post('/ask', async (req, res) => {
+  const { question } = req.body;
   if (!question) return res.status(400).json({ error: 'Question requise' });
 
   try {
     const ragResponse = await chain.invoke({
       context: [...docs],
       input: question
-    })
+    });
 
-    res.json({ answer: ragResponse })
+    res.json({ answer: ragResponse });
   } catch (error) {
-    console.error(err);
-    res.status(500).json({ error: "Erreur serveur" }); 
+    console.error(error);
+    res.status(500).json({ error: "Erreur serveur" });
   }
-})
+});
 
-const port = 5000
+const port = 5000;
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
-  
-})
-
-const response = await chain.invoke({
-  context: [...docs],
-  input: "TAFIKA Seth étudie dans quel université ?",
-})
-
-
+});
--- education/src/components/QuestionResponse.jsx
+++ education/src/components/QuestionResponse.jsx
@@ -12,7 +12,7 @@ export const QuestionResponse = () => {
         setLoading(true);
     
         try {
-          const res = await axios.post('http://localhost:5000/ask', {
+          const res = await axios.post('https://agent-rag-2oaq.onrender.com/ask', {
             question: question.trim(),
           });
 
